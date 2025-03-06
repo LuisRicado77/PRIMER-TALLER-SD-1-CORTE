bb34aaa227abcc293eb357465178fb1e8e45b9db
LuisRicado77 <luissayas2018@gmail.com>
2025-03-06 15:49:10 -0500

    subiendo de nuevo como carpetas

---
+++ ClienteTcpImc/src/cliente/imc/cliente/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cliente.imc.cliente;
+import cliente.imc.cliente.vistas.VentanaPrincipal;
+/**
+ *
+ * @author ldric
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+
+}
---
+++ ServidorTcpImc/src/servidortcpimc/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servidortcpimc;
+import servidortcpimc.views.MainWindow;
+
+/**
+ *
+ * @author ldric
+ */
+public class Principal {
+    public static void main(String[] args) {
+        MainWindow v = new MainWindow();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
